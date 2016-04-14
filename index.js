3622551c5a749baf818b06981dd63510fa1e2d76
Vladimir Agafonkin <agafonkin@gmail.com>
2016-04-14 13:03:23 +0300

    add es6 environment to use typed arrays

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   "extends": "eslint:recommended",
   "env": {
     "node": true,
-    "browser": true
+    "browser": true,
+    "es6": true
   },
   "rules": {
     // disabling unnecessary recommended rules
